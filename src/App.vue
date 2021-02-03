427167f8427707c52539194863f864f3a264880b
Romain Maury <rmaury@dawan.fr>
2021-02-03 10:47:39 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div
+      class='drop-zone'
+      @drop='onDrop($event, 1)' 
+      @dragover.prevent
+      @dragenter.prevent
+    >
+      <div 
+        v-for='item in listOne' 
+        :key='item.title' 
+        class='drag-el' 
+        draggable 
+        @dragstart='startDrag($event, item)'
+      >
+        {{ item.title }}
+      </div>
+    </div>
+
+    <div
+      class='drop-zone'
+      @drop='onDrop($event, 2)' 
+      @dragover.prevent
+      @dragenter.prevent
+    >
+      <div 
+        v-for='item in listTwo' 
+        :key='item.title' 
+        class='drag-el' 
+        draggable 
+        @dragstart='startDrag($event, item)'
+      >
+        {{ item.title }}
+      </div>
+    </div>
+        <div
+      class='drop-zone'
+      @drop='onDrop($event, 3)' 
+      @dragover.prevent
+      @dragenter.prevent
+    >
+      <div 
+        v-for='item in listThree' 
+        :key='item.title' 
+        class='drag-el' 
+        draggable 
+        @dragstart='startDrag($event, item)'
+      >
+        {{ item.title }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+      return {
+        items: [
+        {
+          id: 0,
+          title: 'Item A',
+          list: 1
+        },
+        {
+          id: 1,
+          title: 'Item B',
+          list: 3
+        },
+        {
+          id: 2,
+          title: 'Item C',
+          list: 2
+        }]
+      }
+  },
+  computed: {
+    listOne () {
+      return this.items.filter(item => item.list === 1)
+    },
+    listTwo () {
+      return this.items.filter(item => item.list === 2)
+    },
+    listThree () {
+      return this.items.filter(item => item.list === 3)
+    }
+  },
+  methods: {
+    startDrag: (evt, item) => {
+      evt.dataTransfer.dropEffect = 'move'
+      evt.dataTransfer.effectAllowed = 'move'
+      evt.dataTransfer.setData('itemID', item.id)
+    },
+    onDrop (evt, list) {
+      const itemID = evt.dataTransfer.getData('itemID')
+      const item = this.items.find(item => item.id == itemID)
+      item.list = list
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .drop-zone {
+    background-color: #eee;
+    margin-bottom: 10px;
+    padding: 10px;
+  }
+
+  .drag-el {
+    background-color: #fff;
+    margin-bottom: 10px;
+    padding: 5px;
+  }
 </style>
